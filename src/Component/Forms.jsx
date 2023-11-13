cf22c4bec3c900524fdaa8c42a2d7ff5b8b14498
saharul sifat <126339980+SaharulSifatSoft@users.noreply.github.com>
2023-11-13 11:06:54 +0600

    Forms.jsx

--- src/Component/Forms.jsx
+++ src/Component/Forms.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Forms = () => {
+    const [formObj, setFormObj] =useState({
+        name:""
+    })
+    const InputOnChange =(property, value)=>{
+        setFormObj((prevObj) => ({
+            ...prevObj,
+            [property]: value
+        }));
+    }
+    const FormSubmit=(e)=>{
+        e.preventDefault();
+        alert(JSON.stringify(formObj))
+    }
+
     return (
         <div className="justify-center ">
            <h1>SUBSCRIBE</h1>
             <h1>Subscribe to get the latest news about us</h1>
             <p>Please drop your email below to get daily update about what we do</p>
-            <input className="input mt-10 border-blue-500" type="text"/>
-            <button className="btn-primary p-2">Submit</button>
+            <input onChange={(e)=>{InputOnChange("name",e.target.value)}} value={FormObj.fName} className="input mt-10 border-blue-500" type="text"/>
+            <button onClick={FormSubmit} className="btn-primary p-2">Submit</button>
         </div>
     );
 };
